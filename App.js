a31c67874ffbbcbd0056725128cab64765209fa5
inteligenciaEC <diealm@gmail.com>
2024-10-11 18:07:31 -0700

    mealAI prototype

--- App.js
+++ App.js
@@ -1,9 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ComponentCompiler from './src/ComponentCompiler';
+import {Haus, Calendar, MealAI, Profile, Rewards, CameraScreen, ResultScreen, FixMealCameraScreen, FixMealResultScreen} from './src/components/screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
 import CustomLoadingScreen from './src/Loading/CustomLoadingScreen'; // Importar la pantalla de carga personalizada
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+
+const Tab = createBottomTabNavigator();
+const MealStack = createStackNavigator(); // Stack for the MealAI navigation
+
+function MealAIStack() {
+  return (
+    <MealStack.Navigator initialRouteName="MealMenu">
+      <MealStack.Screen
+        name="MealMenu"
+        component={MealAI}
+       
+      />
+      <MealStack.Screen
+        name="CameraScreen"
+        component={CameraScreen}
+        options={{ headerShown: false }} // Hide header for the camera screen
+      />
+      <MealStack.Screen
+        name="ResultScreen"
+        component={ResultScreen}
+        
+      />
+      <MealStack.Screen name="FixMealCameraScreen" component={FixMealCameraScreen} options={{ headerShown: false }}  />
+      <MealStack.Screen name="FixMealResultScreen" component={FixMealResultScreen} />
+    </MealStack.Navigator>
+  );
+}
+
+const screenOptions = {
+tabBarShowLabel: false,
+headerShown: false,
+tabBarStyle: {
+  position: 'absolute',
+  bottom:0,
+  left:0,
+  right:0,
+  elevation:0,
+  height: 60,
+  background: '#ffffff',
+
+}
+};
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -21,17 +66,82 @@ export default function App() {
   }, []);
 
   // Si las fuentes no están cargadas, muestra la pantalla de carga personalizada
-  if (!fontsLoaded) {
-    return <CustomLoadingScreen />;
-  }
+  // if (!fontsLoaded) {
+  //   return <CustomLoadingScreen />;
+  // }
 
   // Si las fuentes están cargadas, renderiza la app normalmente
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: 'Aleo', fontSize: 20 }}>Welcome to your app using Aleo font!</Text>
-      <ComponentCompiler />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen 
+        name="Haus" 
+        component={Haus} 
+        options={{
+          tabBarIcon: ({focused}) => {
+           return ( <View style={{alignItems:"center", justifyContent:"center"}}>
+           <FontAwesome6 name="house" size={24} color="black" />
+              <Text style={{ fontSize:12, color: focused ? '#000000' : '#666666', }}>Haus</Text>
+            </View>
+           )
+          }
+        }}
+        />
+        <Tab.Screen 
+        name="Calendar" 
+        component={Calendar}
+         options={{
+          tabBarIcon: ({focused}) => {
+           return ( <View style={{alignItems:"center", justifyContent:"center"}}>
+           <FontAwesome6 name="calendar-day" size={24} color="black" />
+              <Text style={{ fontSize:12, color: focused ? '#000000' : '#666666', }}>Calendar</Text>
+            </View>
+           )
+          }
+        }}
+        />
+        <Tab.Screen 
+        name="MealAI" 
+        component={MealAIStack}  // Use the stack navigator for the MealAI tab
+         options={{
+          tabBarIcon: ({focused}) => {
+           return ( <View style={{alignItems:"center", justifyContent:"center"}}>
+           <FontAwesome6 name="bowl-food" size={24} color="black" />
+              <Text style={{ fontSize:12, color: focused ? '#000000' : '#666666', }}>MealAI</Text>
+            </View>
+           )
+          }
+        }}
+        />
+        <Tab.Screen 
+        name="Rewards" 
+        component={Rewards}
+         options={{
+          tabBarIcon: ({focused}) => {
+           return ( <View style={{alignItems:"center", justifyContent:"center"}}>
+           <FontAwesome6 name="ranking-star" size={24} color="black" />
+              <Text style={{ fontSize:12, color: focused ? '#000000' : '#666666', }}>Rewards</Text>
+            </View>
+           )
+          }
+        }}
+        />
+        <Tab.Screen 
+        name="Profile" 
+        component={Profile}
+         options={{
+          tabBarIcon: ({focused}) => {
+           return ( <View style={{alignItems:"center", justifyContent:"center"}}>
+           <FontAwesome6 name="user-large" size={24} color="black" />
+              <Text style={{ fontSize:12, color: focused ? '#000000' : '#666666', }}>Profile</Text>
+            </View>
+           )
+          }
+        }}
+        />
+        
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/components/screens/ResultScreen.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, Button, StyleSheet, ActivityIndicator, ScrollView } from 'react-native';
+
+const ResultScreen = ({ navigation, route }) => {
+    const { imageUri } = route.params;
+    const [apiResponse, setApiResponse] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const fetchMealRecognition = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('mealImage', {
+                uri: imageUri,
+                name: 'meal.jpg',
+                type: 'image/jpg',
+            });
+            formData.append('export', 'json');
+
+            const response = await fetch('http://10.128.195.186:3000/api/recognize-meal', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                body: formData,
+            });
+
+            const data = await response.json();
+            console.log('API Response:', data);
+            const parsedResponse = data.response || data;
+
+            setApiResponse(parsedResponse);
+            setLoading(false);
+        } catch (err) {
+            console.log('Error:', err);
+            setError('Error fetching meal recognition');
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchMealRecognition();
+    }, []);
+
+    if (loading) {
+        return <ActivityIndicator size="large" color="#0000ff" />;
+    }
+
+    return (
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.contentContainer}>
+                <Text style={styles.heading}>What is this?</Text>
+                {imageUri && (
+                    <Image source={{ uri: imageUri }} style={{ width: '100%', height: 200, marginBottom: 20 }} />
+                )}
+
+                {apiResponse ? (
+                    apiResponse.isRecognized === 'true' && apiResponse.isMeal === 'true' ? (
+                        <View>
+                            <Text style={styles.title}>{apiResponse.mealName}</Text>
+                            <Text style={styles.description}>{apiResponse.fullDescription}</Text>
+
+                            <Text style={styles.subtitle}>Ingredients:</Text>
+                            {apiResponse.ingredients.map((item, index) => (
+                                <View key={index} style={styles.ingredientContainer}>
+                                    <Text style={styles.bulletPoint}>• {item.name}: {item.qty}</Text>
+                                    <Text style={styles.note}>{item.note}</Text>
+                                </View>
+                            ))}
+
+                            <Text style={styles.subtitle}>Recipe:</Text>
+                            <Text>{apiResponse.recipe}</Text>
+                        </View>
+                    ) : (
+                        <View>
+                            <Text style={styles.errors}>Sorry, we couldn't recognize the meal.</Text>
+                        </View>
+                    )
+                ) : (
+                    <Text>{error}</Text>
+                )}
+
+                {/* Buttons for Retrying or Navigating back */}
+                <View style={styles.buttonContainer}>
+                    <Button title="Retake Picture" onPress={() => navigation.navigate('CameraScreen')} />
+                    <Button
+                        title="Go Back to Meal-AI"
+                        onPress={() => navigation.reset({
+                            index: 0,
+                            routes: [{ name: 'MealAI' }],
+                        })}
+                    />
+                </View>
+            </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    scrollContainer: {
+        flexGrow: 1, // Ensure ScrollView takes up available space
+        justifyContent: 'space-between', // Ensure content and buttons are spaced correctly
+    },
+    contentContainer: {
+        padding: 20,
+    },
+    errors: {
+        color: 'red',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    heading: {
+        paddingTop: 20,
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    description: {
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+    ingredientContainer: {
+        marginBottom: 5,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+    },
+    bulletPoint: {
+        fontSize: 16,
+        marginRight: 10,
+    },
+    note: {
+        fontStyle: 'italic',
+        marginLeft: 10, // For indentation under the bullet point
+    },
+    buttonContainer: {
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+        paddingBottom: 50, // Add padding to prevent the buttons from being cut off
+        marginBottom: 20,
+    },
+});
+
+export default ResultScreen;
