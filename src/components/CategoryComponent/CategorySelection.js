3bceca2690605e14cf8d0e9f5c653b829d2d4992
arynegi <99070607+arynegi@users.noreply.github.com>
2024-11-08 12:54:28 -0800

    feat: task card icon update

--- src/components/CategoryComponent/CategorySelection.js
+++ src/components/CategoryComponent/CategorySelection.js
@@ -65,7 +65,7 @@ const CategorySelection = ({setCategory}) => {
               isSelected={selectedCategory === category.label}
               onPress={() => {
                 setSelectedCategory(category.label);
-                setIsExpanded(false); 
+                setIsExpanded(true); 
                 setCategory(category.label)
               }}
             />
