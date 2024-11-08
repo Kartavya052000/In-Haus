78d213f70295dd9905e13683b55c38b0f7d98c11
arynegi <99070607+arynegi@users.noreply.github.com>
2024-11-07 16:41:28 -0800

    Scrum 129 header issue calender reward (#13)
    * feat: header
    
    color change
    
    * feat: styling issues fix
    
    styling issues fix
    
    ---------
    
    Co-authored-by: Kartavya Bhayana <93261261+Kartavya052000@users.noreply.github.com>

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ const client = new ApolloClient({
   // uri: 'http://10.128.226.175:4000/api/graphql', // Your GraphQL endpoint
 
   // uri: 'http://98.81.234.60/api/graphql', // Your GraphQL endpoint
-  uri: 'http://192.168.1.174:4000/graphql', // Your GraphQL endpoint
+  uri: 'http://172.20.10.2:4000/graphql', // Your GraphQL endpoint
 
   cache: new InMemoryCache(),
   headers: {
@@ -107,9 +107,11 @@ export default function App() {
 
 
 
+
   return (
     <ApolloProvider client={client}>
       <ShoppingListProvider>
+
         <NavigationContainer>
           <Stack.Navigator initialRouteName="WelcomePage">
             <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
---
+++ src/components/CategoryComponent/CategorySelection.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { CleaningIcon, LaundryIcon, DishesIcon, HomeworkIcon, GroceriesIcon, BuyIcon, CloseIcon, OpenIcon } from '../icons/icons';
+import Typography from '../typography/Typography'; // Ensure the path is correct
+
+const CategoryButton = ({ label, Icon, isSelected, onPress }) => {
+  return (
+    <TouchableOpacity 
+      style={[styles.button, isSelected && styles.selectedButton]} 
+      onPress={onPress}
+    >
+      <Icon color={isSelected ? "#476BFB" : "#333232"} />
+      <Typography variant="BodyS" style={[styles.label, isSelected && styles.selectedLabel]}>
+        {label}
+      </Typography>
+    </TouchableOpacity>
+  );
+};
+
+const CategorySelection = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const categories = [
+    { label: 'Cleaning', Icon: CleaningIcon },
+    { label: 'Laundry', Icon: LaundryIcon },
+    { label: 'Dishes', Icon: DishesIcon },
+    { label: 'Homework', Icon: HomeworkIcon },
+    { label: 'Groceries', Icon: GroceriesIcon },
+    { label: 'Buy', Icon: BuyIcon },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {/* Header with expand/collapse icon */}
+      <TouchableOpacity onPress={() => setIsExpanded(!isExpanded)} style={styles.headerContainer}>
+        <Typography variant="SH4" style={styles.headerText}>
+          Category
+        </Typography>
+        <View style={styles.icon}>
+          {isExpanded ? <CloseIcon /> : <OpenIcon />}
+        </View>
+      </TouchableOpacity>
+
+      {/* Display selected category and a divider below when not expanded */}
+      {!isExpanded && selectedCategory && (
+        <>
+          <View style={styles.selectedCategoryContainer}>
+            <Typography variant="BodyS" style={styles.selectedCategoryText}>
+              {selectedCategory}
+            </Typography>
+          </View>
+          <View style={styles.divider} />
+        </>
+      )}
+
+      {/* Category buttons only show when expanded */}
+      {isExpanded && (
+        <View style={styles.categoryContainer}>
+          {categories.map((category, index) => (
+            <CategoryButton
+              key={index}
+              label={category.label}
+              Icon={category.Icon}
+              isSelected={selectedCategory === category.label}
+              onPress={() => {
+                setSelectedCategory(category.label);
+                setIsExpanded(false); 
+              }}
+            />
+          ))}
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // paddingVertical: 10,
+    // borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  headerText: {
+    color: '#333232',
+  },
+  icon: {
+    marginLeft: 10,
+  },
+  selectedCategoryContainer: {
+    paddingVertical: 10,
+  },
+  selectedCategoryText: {
+    color: '#6F6D6D',
+  },
+  divider: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginTop: 5,
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  button: {
+    width: '30%',
+    padding: 10,
+    backgroundColor: '#FCFAFA',
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 10,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  selectedButton: {
+    borderColor: '#476BFB',
+    borderWidth: 2,
+    backgroundColor: '#FFFFFF',
+  },
+  label: {
+    marginTop: 8,
+    color: '#333232',
+    fontSize: 14,
+  },
+  selectedLabel: {
+    color: '#476BFB',
+  },
+});
+
+export default CategorySelection;
