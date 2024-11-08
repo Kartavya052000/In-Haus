f3e30e75578542aebca6e502883e987a5d46bb7e
matt7908 <mbuitrago00@mylangara.ca>
2024-11-08 15:10:12 -0800

    update button

--- App.js
+++ App.js
@@ -60,7 +60,7 @@ const client = new ApolloClient({
   // uri: 'http://10.128.226.175:4000/api/graphql', // Your GraphQL endpoint
 
   // uri: 'http://98.81.234.60/api/graphql', // Your GraphQL endpoint
-  uri: 'http://192.168.1.249:4000/graphql', // Your GraphQL endpoint
+  uri: 'http://10.128.231.24:4000/graphql', // Your GraphQL endpoint
 
   cache: new InMemoryCache(),
   headers: {
