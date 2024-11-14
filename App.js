a7de41c18973071d2a61987bfa22257419c8f363
matt7908 <146280868+matt7908@users.noreply.github.com>
2024-11-14 15:58:10 -0800

    up (#14)
    * up
    
    * Submit
    
    ---------
    
    Co-authored-by: Kartavya Bhayana <93261261+Kartavya052000@users.noreply.github.com>

--- App.js
+++ App.js
@@ -60,7 +60,11 @@ const client = new ApolloClient({
   uri: 'http://10.128.226.175:4000/graphql', // Your GraphQL endpoint
 
   // uri: 'http://98.81.234.60/api/graphql', // Your GraphQL endpoint
-  // uri: 'http://192.168.1.249:4000/graphql', // Your GraphQL endpoint
+
+
+  uri: 'http://10.128.243.115:4000/graphql', // Your GraphQL endpoint
+
+
 
   cache: new InMemoryCache(),
   headers: {
