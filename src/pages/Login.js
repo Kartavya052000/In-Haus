c344792102184b81d73a63a8239c9fd216654d84
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-12-13 00:07:40 -0800

    capstone commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -28,7 +28,7 @@ const storeToken = async (token, points,id) => {
 };
 
 const Login = () => {
-  const [email, setEmail] = useState('JuanDi@gmail.com');  // expo build
+  const [email, setEmail] = useState('Deigo@gmail.com');  // expo build
   // const [email, setEmail] = useState('Aryan@gmail.com'); // simulator
   const [password, setPassword] = useState('password123');
   const [showPassword, setShowPassword] = useState(false); // Estado para controlar visibilidad de la contraseña
